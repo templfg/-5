b8240bf7937b814edd65930fe4f5aaa495457a0b
Артём <156431980+templfg@users.noreply.github.com>
2024-02-16 15:22:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("4Sin(x) * 5Cos(x)=" + Convert.ToString(4 * Math.Sin(x) * 5 *Math.Cos(x)));
+            Console.ReadKey();
+        }
+    }
+}
